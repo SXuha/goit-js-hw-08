c8095d85c8a4cf908f216d34e965478ed6eb1c7a
Oksana Brunko <101710955+SXuha@users.noreply.github.com>
2022-07-10 13:49:26 +0300

    Update 02-video.js

--- js/02-video.js
+++ js/02-video.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+function savePlayerRuntime(data) {
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+
+  if (data.seconds === data.duration) {
+    localStorage.removeItem(STORAGE_KEY);
+  }
+}
+
+player.on('timeupdate', throttle(savePlayerRuntime, 1000));
+
+const currentVideoRuntime = localStorage.getItem(STORAGE_KEY);
+
+player
+  .setCurrentTime(currentVideoRuntime)
+  .then(function (seconds) {
+    seconds = currentVideoRuntime;
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        seconds <= 0;
+        break;
+
+      default:
+        console.log('Locale storage clear');
+        break;
+    }
+  });
